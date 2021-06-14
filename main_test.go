167c1b5069cf832dcb14f8b41bafa90fd4b047aa
Ruslan Kolegov <marsredskies@gmail.com>
2021-06-14 18:53:19 +0600

    converted bytes to int

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,22 +14,22 @@ func TestHandleCalc(t *testing.T) {
 	testCases := []struct {
 		name string
 		num  int
-		want []byte
+		want int
 	}{
 		{
 			name: "zero",
 			num:  5,
-			want: []byte("25"),
+			want: 25,
 		},
 		{
 			name: "one",
 			num:  12,
-			want: []byte("144"),
+			want: 144,
 		},
 		{
 			name: "two",
 			num:  100,
-			want: []byte("10000"),
+			want: 10000,
 		},
 	}
 
@@ -39,7 +40,8 @@ func TestHandleCalc(t *testing.T) {
 			rec := httptest.NewRecorder()
 			request, _ := http.NewRequest("GET", fmt.Sprintf("/calc?n=%d", cases.num), nil)
 			handler.ServeHTTP(rec, request)
-			assert.Equal(t, cases.want, rec.Body.Bytes())
+			got, _ := strconv.Atoi(string(rec.Body.Bytes()))
+			assert.Equal(t, cases.want, got)
 		})
 	}
 
